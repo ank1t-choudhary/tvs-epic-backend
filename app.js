c89415056f2a1c16f8368b9e54f9367545db9dde
Ankit Choudhary <choudharyankit135aj@gmail.com>
2023-10-18 18:47:19 +0530

    changes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ process.env.TZ = "Asia/Calcutta";
 
 const app = require("./src/server");
 
-app.listen(process.env.PORT || 8000, "0.0.0.0", async () => {
+app.listen(process.env.PORT || 80, "0.0.0.0", async () => {
     console.log(
-        "Server running at http://localhost:" + (process.env.PORT || 8000)
+        "Server running at http://localhost:" + (process.env.PORT || 80)
     );
 });
